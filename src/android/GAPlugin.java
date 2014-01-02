4df9d35ea3220ad49ed0bcfae4fcde55fefb1e6c
Gen Liu <gerald_721@hotmail.com>
2014-01-02 18:51:31 +0800

    change to easy tracker

--- src/android/GAPlugin.java
+++ src/android/GAPlugin.java
@@ -4,21 +4,18 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 
-import com.google.analytics.tracking.android.GAServiceManager;
-import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.MapBuilder;
 import com.google.analytics.tracking.android.Tracker;
 
 public class GAPlugin extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callback) {
-		GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
-		Tracker tracker = ga.getDefaultTracker(); 
+		Tracker tracker = EasyTracker.getInstance(cordova.getActivity());
 
 		if (action.equals("initGA")) {
 			try {
-				tracker = ga.getTracker(args.getString(0));
-				GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));
-				ga.setDefaultTracker(tracker);
 				callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
 				return true;
 			} catch (final Exception e) {
@@ -26,7 +23,6 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("exitGA")) {
 			try {
-				GAServiceManager.getInstance().dispatch();
 				callback.success("exitGA");
 				return true;
 			} catch (final Exception e) {
@@ -34,7 +30,16 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("trackEvent")) {
 			try {
-				tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));
+				// v3
+				tracker.send(MapBuilder.createEvent(
+					args.getString(0),
+					args.getString(1),
+					args.getString(2),
+					args.getLong(3)
+				).build());
+				
+				// v2
+				// tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));
 				callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
 				return true;
 			} catch (final Exception e) {
@@ -42,7 +47,12 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("trackPage")) {
 			try {
-				tracker.sendView(args.getString(0));
+				// v3
+				tracker.set(Fields.SCREEN_NAME, args.getString(0));
+				tracker.send(MapBuilder.createAppView().build());
+				
+				// v2
+				//tracker.sendView(args.getString(0));
 				callback.success("trackPage - url = " + args.getString(0));
 				return true;
 			} catch (final Exception e) {
@@ -50,7 +60,11 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("setVariable")) {
 			try {
-				tracker.setCustomDimension(args.getInt(0), args.getString(1));
+				// v3
+				tracker.set(Fields.customDimension(args.getInt(0)), args.getString(1));
+				
+				// v2
+				//tracker.setCustomDimension(args.getInt(0), args.getString(1));
 				callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
 				return true;
 			} catch (final Exception e) {
@@ -59,7 +73,11 @@ public class GAPlugin extends CordovaPlugin {
 		}
 		else if (action.equals("setDimension")) {
 			try {
-				tracker.setCustomDimension(args.getInt(0), args.getString(1));
+				// v3
+				tracker.set(Fields.customDimension(args.getInt(0)), args.getString(1));
+				
+				// v2
+				//tracker.setCustomDimension(args.getInt(0), args.getString(1));
 				callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
 				return true;
 			} catch (final Exception e) {
@@ -68,7 +86,11 @@ public class GAPlugin extends CordovaPlugin {
 		}
 		else if (action.equals("setMetric")) {
 			try {
-				tracker.setCustomMetric(args.getInt(0), args.getLong(1));
+				// v3
+				tracker.set(Fields.customMetric(args.getInt(0)), args.getString(1));
+				
+				// v2
+				//tracker.setCustomMetric(args.getInt(0), args.getLong(1));
 				callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
 				return true;
 			} catch (final Exception e) {
@@ -78,4 +100,3 @@ public class GAPlugin extends CordovaPlugin {
 		return false;
 	}
 }
-
